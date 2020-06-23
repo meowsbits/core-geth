2313916940621fd2b72f981a03a91b78b084b7ea
meows <b5c6@protonmail.com>
2020-06-23 13:01:56 -0500

    utils: reset rlp stream with each batch
    this might help with the oom?
    
    Signed-off-by: meows <b5c6@protonmail.com>

--- cmd/utils/cmd.go
+++ cmd/utils/cmd.go
@@ -164,6 +164,7 @@ func ImportChain(chain *core.BlockChain, fn string) error {
 		if _, err := chain.InsertChain(missing); err != nil {
 			return fmt.Errorf("invalid block %d: %v", n, err)
 		}
+		stream.Reset(reader, 0)
 	}
 	return nil
 }
