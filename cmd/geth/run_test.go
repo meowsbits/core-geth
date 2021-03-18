380e6a5315bcd1a810b5784e8b99c34c6281be53
meows <b5c6@protonmail.com>
2021-03-18 13:09:57 -0500

    :hammer:

--- cmd/geth/run_test.go
+++ cmd/geth/run_test.go
@@ -61,7 +61,7 @@ func TestMain(m *testing.M) {
 	if reexec.Init() {
 		return
 	}
-	m.Run()
+	os.Exit(m.Run())
 }
 
 // spawns geth with the given command line args. If the args don't set --datadir, the
