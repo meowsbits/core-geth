184b75a9616ca6f5928817cca7ced5e2f841462d
meows <b5c6@protonmail.com>
2021-10-25 18:48:53 -0700

    core: still fixing the fix
    Date: 2021-10-25 18:48:53-07:00
    Signed-off-by: meows <b5c6@protonmail.com>

--- core/blockchain_insert.go
+++ core/blockchain_insert.go
@@ -23,7 +23,6 @@ import (
 	"github.com/ethereum/go-ethereum/common/mclock"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/log"
-	"github.com/ethereum/go-ethereum/params/vars"
 )
 
 // insertStats tracks and reports on block insertion.
@@ -61,7 +60,7 @@ func (st *insertStats) report(chain []*types.Block, index int, dirty common.Stor
 		context := []interface{}{
 			"blocks", st.processed, "txs", txs, "mgas", float64(st.usedGas) / 1000000,
 			"elapsed", common.PrettyDuration(elapsed), "mgasps", float64(st.usedGas) * 1000 / float64(elapsed),
-			"number", end.Number(), "hash", end.Hash(), "tab", st.totalActiveBalance / vars.Ether,
+			"number", end.Number(), "hash", end.Hash(), "tab", st.totalActiveBalance,
 		}
 		if timestamp := time.Unix(int64(end.Time()), 0); time.Since(timestamp) > time.Minute {
 			context = append(context, []interface{}{"age", common.PrettyAge(timestamp)}...)
